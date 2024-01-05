81c3323875fecc7e0a9078dcb45b0b37268eb8ea
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-05 04:57:55 +0100

    Add Paschal Triangle Task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Paschal Triangle Module
+"""
+
+
+def pascal_triangle(n):
+    """paschal_triangle function"""
+    triangle_list = []
+    if n > 0:
+        for i in range(n):
+            row = [1] * (i + 1)
+            if i >= 2:
+                for j in range(1, i):
+                    row[j] = triangle_list[i - 1][j - 1] + triangle_list[i - 1][j]
+            triangle_list.append(row)
+    return triangle_list
---
+++ README.md
@@ -0,0 +1,2 @@
+About Repo
+ALX SWE Interview
