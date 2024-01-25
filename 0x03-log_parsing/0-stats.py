2fea3ded971a898097cbc14d6613c9814e8764a9
Leonard Nzekwe <nzekweleo@gmail.com>
2024-01-25 23:49:38 +0100

    Update Log Parsing Project Task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
-"""Log Parsing Module"""
-import sys
+"""A script that reads stdin line by line and computes metrics"""
+
+from sys import stdin
+
+status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_file_size = 0
+line_count = 0
 
 
-def print_statistics(total_size, status_codes):
-    """Handles statistics printing"""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def print_statistics():
+    """Prints computed statistics"""
+    print("File size:", total_file_size)
+    for key, value in status_dict.items():
+        if value:
+            print("{}: {}".format(key, value))
 
 
-def parse_line(line, total_size, status_codes):
-    """parses stdin line"""
+def parse_line(line):
+    """Parses a line and updates metrics"""
+    global total_file_size, line_count
+
+    line_count += 1
+    parts = line.split()
+
     try:
-        parts = line.split(" ")
-        status_code = int(parts[-2])
         file_size = int(parts[-1])
-        total_size += file_size
-
-        if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-            if status_code not in status_codes:
-                status_codes[status_code] = 0
-            status_codes[status_code] += 1
-
-    except (ValueError, IndexError):
-        pass
-
-    return total_size, status_codes
-
-
-def main():
-    """main program"""
-    total_size = 0
-    status_codes = {}
+        total_file_size += file_size
+    except (IndexError, ValueError, TypeError):
+        return
 
     try:
-        for i, line in enumerate(sys.stdin, 1):
-            total_size, status_codes = parse_line(
-                line.strip(), total_size, status_codes
-            )
+        status_code = int(parts[-2])
+        if status_code in status_dict:
+            status_dict[status_code] += 1
+    except (IndexError, ValueError, TypeError):
+        return
 
-            if i % 10 == 0:
-                print_statistics(total_size, status_codes)
-
-    except KeyboardInterrupt:  # (CTRL + C)
-        pass  # Allow KeyboardInterrupt to exit gracefully
-
-    print_statistics(total_size, status_codes)
+    if line_count == 10:
+        print_statistics()
+        line_count = 0
 
 
-if __name__ == "__main__":
-    main()
+try:
+    for line in stdin:
+        parse_line(line.strip())
+
+    print_statistics()
+
+except KeyboardInterrupt:
+    print_statistics()
