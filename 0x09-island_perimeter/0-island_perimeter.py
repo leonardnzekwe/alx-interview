d24cbbb94bb2633690bf1d59713addebd55f76d9
Leonard Nzekwe <nzekweleo@gmail.com>
2024-03-04 09:15:38 +0100

    Add Interview Project: Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+island_perimeter(grid) Module:
+that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter function:
+    that returns the perimeter of the island described in grid
+
+    Args:
+        grid (list of list of integers): a list of list of integers
+
+    Returns:
+        int: perimeter of the island described in grid
+    """
+    row_len = len(grid)
+    col_len = len(grid[0])
+    perimeter = 0
+
+    for row in range(row_len):
+        for col in range(col_len):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,2 @@
+# Island Perimeter
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
